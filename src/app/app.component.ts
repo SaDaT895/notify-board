91e4061f51226b82d30d8a3bb7e0d168838b3972
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-04-26 16:07:52 +0200

    feat: initialise Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule,
+    MatToolbarModule,
+    RouterOutlet,
+    MatButtonModule,
+    RouterModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'notify-board';
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { NotificationListComponent } from './notification-list/notification-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'notifications', pathMatch: 'full' },
+  { path: 'notifications', component: NotificationListComponent },
+];
