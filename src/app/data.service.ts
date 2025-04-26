fbd4206c55faaabba772937cd368b9616c3f54b7
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-04-26 16:52:07 +0200

    feat: add data service with -in-memory

---
+++ src/app/data.service.ts
@@ -0,0 +1,60 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+export interface Notification {
+  id: number;
+  icon: string;
+  text: string;
+  metadata: string;
+  link?: string;
+}
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DataService {
+  private notifications: Notification[] = [
+    {
+      id: 1,
+      icon: 'check_circle',
+      text: 'Notification 1',
+      metadata: 'Metadata 1',
+      link: 'https://example.com/1',
+    },
+    {
+      id: 2,
+      icon: 'error',
+      text: 'Notification 2',
+      metadata: 'Metadata 2',
+    },
+    {
+      id: 3,
+      icon: 'info',
+      text: 'Notification 3',
+      metadata: 'Metadata 3',
+      link: 'https://example.com/3',
+    },
+  ];
+  private _notifications = new BehaviorSubject<Notification[]>(
+    this.notifications
+  );
+
+  notifications$ = this._notifications.asObservable();
+
+  add(notification: Notification) {
+    notification.id = Date.now();
+    this.notifications.push(notification);
+    this._notifications.next(this.notifications);
+  }
+
+  update(id: number, notification: Notification) {
+    const index = this.notifications.findIndex((n) => n.id === id);
+    this.notifications[index] = { ...notification, id };
+    this._notifications.next(this.notifications);
+  }
+
+  delete(id: number) {
+    this.notifications = this.notifications.filter((n) => n.id !== id);
+    this._notifications.next(this.notifications);
+  }
+}
