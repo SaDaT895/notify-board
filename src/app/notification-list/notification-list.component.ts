d6287f1b59276d4cb48221e29ee051a6e9736668
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-04-26 17:17:54 +0200

    feat: add table for notifications

---
+++ src/app/notification-list/notification-list.component.ts
@@ -0,0 +1,39 @@
+import { CommonModule, AsyncPipe, TitleCasePipe } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { DataService, Notification } from '../data.service';
+import { MatIconModule } from '@angular/material/icon';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { tap } from 'rxjs';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-notification-list',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatTableModule,
+    MatButtonModule,
+    AsyncPipe,
+    TitleCasePipe,
+    MatButtonModule,
+    MatIconModule,
+    MatTooltipModule,
+    RouterModule,
+  ],
+  templateUrl: './notification-list.component.html',
+  styleUrl: './notification-list.component.scss',
+})
+export class NotificationListComponent {
+  private dataService = inject(DataService);
+  notifications$ = this.dataService.notifications$.pipe(tap(console.log));
+
+  columnNames: (keyof Notification)[] = ['id', 'icon', 'text', 'metadata'];
+
+  displayedColumns = [...this.columnNames, 'actions'];
+
+  deleteNotification(id: number) {
+    this.dataService.delete(id);
+  }
+}
