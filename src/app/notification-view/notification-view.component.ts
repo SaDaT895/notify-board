84a34d1237a7c176583d3a171b243b21cd1d7c1c
Sadat Ahmad <sadatahmadofficial@gmail.com>
2025-04-27 13:43:59 +0200

    feat: handle large text

--- src/app/notification-view/notification-view.component.ts
+++ src/app/notification-view/notification-view.component.ts
@@ -1,14 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
-import { CommonModule } from '@angular/common';
+import { CommonModule, SlicePipe } from '@angular/common';
 import { Notification } from '../data.service';
 import { MatDividerModule } from '@angular/material/divider';
-import { UrlPipe } from '../url.pipe';
 
 @Component({
   selector: 'app-notification-view',
-  imports: [MatCardModule, MatIconModule, CommonModule, MatDividerModule],
+  imports: [
+    MatCardModule,
+    MatIconModule,
+    CommonModule,
+    MatDividerModule,
+    SlicePipe,
+  ],
   standalone: true,
   templateUrl: './notification-view.component.html',
   styleUrl: './notification-view.component.scss',
